1c6eca14df0eced036d42d7f7e4cc3c34284cd89
Pendragon215 <111545340+Pendragon215@users.noreply.github.com>
2022-08-25 16:29:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string stg = "Hello, World! (2)";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username");
+            string username = Console .ReadLine();
+            Console.WriteLine("Username is: "+ username);
+
+            Console.WriteLine("Enter user's age");
+            int age = Convert. ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is: " + username + " and the age is: "+ age);
+
+        }
+    }
+}
